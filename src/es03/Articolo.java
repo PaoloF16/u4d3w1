97928e2683a2b24e368e01ea471dd758998976a0
PaoloF16 <paolo.cafis@gmail.com>
2026-06-04 19:48:43 +0200

    f/ es03 finished

--- src/es03/Articolo.java
+++ src/es03/Articolo.java
@@ -7,28 +7,52 @@ public class Articolo {
     private String nameArticle;
     private String codeArticle;
     private String description;
-    private String price;
+    private double price;
     private int quantita;
 
     private static ArrayList<Articolo> allArticles = new ArrayList<>();
-
+    /*
+    * Logica de este codigo:
+    * Cree una clase Article
+    * Le di los parametros
+    * Luego cree un array de tipo Article
+    * Genere el nuevo array con todos los articulos creados
+    * Dentro el constructor hice que cada vez que se crea el objeto se añada al array
+    * */
     public Articolo(String nameArticle,double price,int quantita){
         this.nameArticle = nameArticle;
         Random codiceRandom = new Random();
         codeArticle = "#"+codiceRandom.nextInt(100000);
         description = "This is the product: " + nameArticle;
-        this.price = price + "$";
+        this.price = price;
         this.quantita = quantita;
         allArticles.add(this);
 
     }
 
+
     public static void stampaTutte(){
         for( Articolo ListaArticular : allArticles){
             System.out.println(ListaArticular.nameArticle);
-            System.out.println(ListaArticular.price);
         }
     }
+    /*
+    * Teniendo el array con los objetos creados
+    * Hago un enhanced for para recorrer cada nombre de articulos
+    * Los imprimo
+    * */
+    public static double allPrize(){
+      double total = 0;
+      for (Articolo articolo : allArticles){
+          total += articolo.price;
+      }
+      return total;
+    }
+    /*
+    * Hago el metodo para poder sumar el precio total
+    * enhanced for para sumar el precio de cada elemento
+    * */
+
     @Override
     public String toString() {
         return "Articolo{" +
--- src/es03/Ejercicio3.java
+++ src/es03/Ejercicio3.java
@@ -1,14 +1,23 @@
 package es03;
 
 public class Ejercicio3 {
-    public static void main(){
+    public static void main(String[] args){
         var firstArticle = new Articolo("Jeans",30,20);
         var secondArticle = new Articolo("Jacket",20,10);
+        var thirdArticle = new Articolo("T-Shirt",20, 10);
+        var fourthArticle = new Articolo("collar",50,10);
+
+
+
+
         var firstClient = new Cliente("Paolo","Fiorentini","paolo@gmail.com","16-2-2435");
         System.out.println(firstArticle.toString());
         System.out.println(secondArticle.toString());
+        System.out.println(thirdArticle.toString());
+
         System.out.println(firstClient.toString());
         Articolo.stampaTutte();
+        System.out.println(Articolo.allPrize());
     }
 
 }
